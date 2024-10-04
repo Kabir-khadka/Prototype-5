259d2382d518133e7b98669dfabf2755bbf08bda
Kabir-khadka <kabirkhadka50@gmail.com>
2024-10-04 21:26:37 +0545

    Added Spawn Manager

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,6 +4,12 @@ public class Target : MonoBehaviour
 {
 
     private Rigidbody targetRb;
+
+    private float minSpeed = 15;
+    private float maxSpeed = 20;
+    private float maxTorque = 1;
+    private float xSpawnRange = 4;
+    private float ySpawnRange = -6;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,12 +17,44 @@ public class Target : MonoBehaviour
         targetRb = GetComponent<Rigidbody>();
 
         //Adding certain force with random speed in upward direction
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 
+        //Rotating and moving objects
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        //Setting the position for the objects to spawn
+        transform.position = RandomPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomPos()
+    {
+        return new Vector3(Random.Range(-xSpawnRange, xSpawnRange), ySpawnRange);
     }
 }
